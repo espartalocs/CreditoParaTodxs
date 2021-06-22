70b48cd22ad33db7fceaa067f1981da6a596666c
clebeson lima <kleber.sonlima@hotmail.com>
2021-06-22 15:08:08 -0300

    parcelas

---
+++ js/emprestimo.js
@@ -0,0 +1,6 @@
+let emprestimoJSON = [
+    {id:1, emprestimo:'R$ 2.000,00', taxa:'3,97% A.M'} +
+    {id:2, emprestimo:'R$ 5.000,00', taxa:'3,97% A.M'} +
+    {id:3, emprestimo:'R$ 10.000,00', taxa:'3,97% A.M'} +
+    {id:4, emprestimo:'R$ 20.000,00', taxa:'3,97% A.M'} 
+];
--- js/proposta.js
+++ js/proposta.js
@@ -1,1 +1,16 @@
+const c = (el)=>document.querySelector(el);
+const cs= (el)=>document.querySelectorAll(el);
+
+emprestimoJSON.map((item)=>{
+    let emprestimoItem = c('.models .emprestimo-item').cloneNode(true);
+
+     emprestimoItem.querySelector('.empretimo-item--name').innerHTML = item.emprestimo;
+     emprestimoItem.querySelector('.empretimo-item--juros').innerHTML = item.taxa;
+    emprestimoItem.querySelector('a').addEventListner('click', (e)=>{
+        e.preventDefault();
+
+    });
+
+
+});
 
